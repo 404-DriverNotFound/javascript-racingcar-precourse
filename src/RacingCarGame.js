17eec14ac6684420d938bdd74f904bf2ec3739ff
Ykoh <ykoh@student.42seoul.kr>
2021-06-19 01:32:25 +0900

    feat: 시도 횟수를 입력받는 기능 추가

--- src/RacingCarGame.js
+++ src/RacingCarGame.js
@@ -14,12 +14,15 @@ export default class RacingCarGame {
   constructor() {
     this.$carGameContainer = document.querySelector('.car-game-container');
     this.$carNamesInput = document.querySelector('#car-names-input');
+    this.$racingCountInput = document.querySelector('#racing-count-input');
   }
 
   addEventListener() {
     this.$carGameContainer.addEventListener('click', (e) => {
       if (e.target.id === 'car-names-submit') {
         this.onClickCarNamesSubmit();
+      } else if (e.target.id === 'racing-count-submit') {
+        this.onClickRacingCountSubmit();
       }
     });
   }
@@ -27,6 +30,17 @@ export default class RacingCarGame {
   onClickCarNamesSubmit() {
     const carNames = this.$carNamesInput.value;
     if (isValidCarNames(carNames)) {
+      // eslint-disable-next-line no-alert
+      alert('제대로 입력했군!');
+    } else {
+      // eslint-disable-next-line no-alert
+      alert('제대로 입력하라');
+    }
+  }
+
+  onClickRacingCountSubmit() {
+    const racingCount = this.$racingCountInput.value;
+    if (racingCount > 0) {
       // eslint-disable-next-line no-alert
       alert('제대로 입력했군!');
     } else {
